7873755412fa27e8b32f86670890cfe6d386c628
Alahjah Green <green.alahjah38@gmail.com>
2024-09-26 10:03:56 -0400

    updates to js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const intianlsCards = [
+const intianlCards = [
   {
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -27,4 +27,4 @@ const intianlsCards = [
   },
 ];
 
-console.log = intianlsCards[0];
+console.log = intianlCards[1];
