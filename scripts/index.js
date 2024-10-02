14be909df83da8e1327e56d67f82283da3a27946
Alahjah Green <green.alahjah38@gmail.com>
2024-10-02 02:55:28 -0400

    change and update js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
-var initialCards = [
+const initialCards = [
   {
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
+
   {
     title: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
+
   {
     title: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
+
   {
     title: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
